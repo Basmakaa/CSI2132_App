40035f1f5a33907ed40bb59fa7ac3a05c1c3a11c
Guy L <g.guy.lab@gmail.com>
2022-03-13 19:47:46 -0400

    Removed server submodule

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	var socket = io.connect('https://csi2132-group12.herokuapp.com/');
 	
+	alert("test");
+	
 	socket.on('connect', function(){
 		
 		socket.emit('mycoolevent', {myattribute: 15});
 				
 	});
-}
+});
