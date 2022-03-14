70ddb81c0fbb708b1f2be1804e34e104350c4403
Guy L <g.guy.lab@gmail.com>
2022-03-14 12:58:44 -0400

    Fixed socket.io issue

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-	var socket = io.connect('http://csi2132-group12.herokuapp.com/');
-	
-	alert("test");
-	
-	/*socket.on('connect', function(){
+	var socket = io.connect('https://csi2132-group12.herokuapp.com/');
+		
+	socket.on('connect', function(){
+		alert("connected");
 		socket.emit('mycoolevent', {myattribute: 15});	
-	});*/
+	});
 });
