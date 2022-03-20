3ad41d8b57258217590b8882cec680059fcbe99b
Guy L <g.guy.lab@gmail.com>
2022-03-20 00:03:43 -0400

    Added funct. to retrieve user's appointments

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var socket;
+var future_appts;
 
 $(document).ready(function() {
 	socket = io.connect('https://csi2132-group12.herokuapp.com/');
@@ -12,6 +13,19 @@ $(document).ready(function() {
 		if(data.status == 'error') console.log(data.reason);
 		else{	
 			console.log("Logged in as " + data.name + ".");
+			
+			//Fetch upcoming appointments.
+			//Note: could be replaced in the future with a general funct. to retrieve all data necessary to be displayed post-login.
+			socket.emit('fetch_future_appointments');
+		}
+	});
+	
+	//Response to upcoming appointments request.
+	socket.on('fetch_future_appointments_res', function(data) {
+		if(data.status == 'error') console.log(data.reason);
+		else{	
+			console.log("Upcoming appointments fetched.");
+			future_appts = data.result;
 		}
 	});
 });
