2526d00a3e9a163d0f6bbf7f8a14ebbb55c41ab1
Basma Kaanane <bkaan006@uottawa.ca>
2022-03-29 20:27:37 -0400

    Register/login front end Part1

--- js/index.js
+++ js/index.js
@@ -7,7 +7,38 @@ $(document).ready(function() {
 	socket.on('connect', function(){
 		console.log("Connected to the server.");
 	});
+    
+	//hide and show for register and login 
+	$(".wrapper").addClass("active");
+	// $(".after_login").hide();
+
+	$(".sign_up").hide();
+	$(".sign_in_li").addClass("active");
 	
+	$(".sign_up_li").click(function(){
+	  $(this).addClass("active");
+	  $(".sign_in_li").removeClass("active");
+	  $(".sign_up").show();
+	   $(".sign_in").hide();
+	})
+	
+	$(".sign_in_li").click(function(){
+	  $(this).addClass("active");
+	  $(".sign_up_li").removeClass("active");
+	  $(".sign_in").show();
+	   $(".sign_up").hide();
+	})
+
+	//Response from Register attempts
+	socket.on('register_res', function(data) {
+		if(data.status == 'error') console.log(data.reason);
+		else{	
+			console.log("You have been registered successfully");
+			socket.emit('login_res');
+		}
+	});  
+
+
 	//Response from login attempts
 	socket.on('login_res', function(data) {
 		if(data.status == 'error') console.log(data.reason);
