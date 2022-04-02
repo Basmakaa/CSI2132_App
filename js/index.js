5aab9e48377bb2784f7f68300b3bd006393dd341
Guy L <g.guy.lab@gmail.com>
2022-04-02 18:04:26 -0400

    UI overhaul, added section swapping and login functions

--- js/index.js
+++ js/index.js
@@ -7,37 +7,6 @@ $(document).ready(function() {
 	socket.on('connect', function(){
 		console.log("Connected to the server.");
 	});
-    
-	//hide and show for register and login 
-	$(".wrapper").addClass("active");
-	// $(".after_login").hide();
-
-	$(".sign_up").hide();
-	$(".sign_in_li").addClass("active");
-	
-	$(".sign_up_li").click(function(){
-	  $(this).addClass("active");
-	  $(".sign_in_li").removeClass("active");
-	  $(".sign_up").show();
-	   $(".sign_in").hide();
-	})
-	
-	$(".sign_in_li").click(function(){
-	  $(this).addClass("active");
-	  $(".sign_up_li").removeClass("active");
-	  $(".sign_in").show();
-	   $(".sign_up").hide();
-	})
-
-	//Response from Register attempts
-	socket.on('register_res', function(data) {
-		if(data.status == 'error') console.log(data.reason);
-		else{	
-			console.log("You have been registered successfully");
-			socket.emit('login_res');
-		}
-	});  
-
 
 	//Response from login attempts
 	socket.on('login_res', function(data) {
@@ -51,6 +20,15 @@ $(document).ready(function() {
 		}
 	});
 	
+	//Response from Register attempts
+	socket.on('register_res', function(data) {
+		if(data.status == 'error') console.log(data.reason);
+		else{	
+			console.log("You have been registered successfully");
+			socket.emit('login_res');
+		}
+	});  
+	
 	//Response to upcoming appointments request.
 	socket.on('fetch_future_appointments_res', function(data) {
 		if(data.status == 'error') console.log(data.reason);
@@ -60,3 +38,18 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function swapTo(section, btn){
+	$("#main section").hide();
+	$("#"+section).show();
+	$(".active").removeClass("active");
+	$(btn).addClass("active");
+}
+
+function login(){
+	socket.emit('login', {email: $("#lgemail").val(), password: $("#lgpass").val()});
+}
+
+function register(){
+	
+}
