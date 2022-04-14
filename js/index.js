9cc93f734afd055ce7c06b695a8c15fb95f78638
Guy L <g.guy.lab@gmail.com>
2022-04-14 00:03:32 -0400

    Registering functionality

--- js/index.js
+++ js/index.js
@@ -6,7 +6,14 @@ var phone_counter = 0;
 
 $(document).ready(function() {
 	//jQuery UI Setup
-	$("#bappt_date").datepicker();
+	$("#rdateofbirth").datepicker({
+        maxDate: new Date(),
+		changeMonth: true,
+		changeYear: true
+    });
+	$("#bappt_date").datepicker({
+        minDate: new Date()
+    });
 	
 	//Socket
 	socket = io.connect('https://csi2132-group12.herokuapp.com/');
@@ -17,14 +24,16 @@ $(document).ready(function() {
 
 	//Response from login attempts
 	socket.on('login_res', function(data) {
-		if(data.status == 'error') console.log(data.reason);
+		if(data.status == 'error') toastNotify(data.reason);
 		else{	
 			$("#navbar h1").text("Hello " + data.name + "!");
 			$("#mlogin").hide();
 			$("#mregister").hide();
 			$("#muinfo").show();
-			//$("#mbappt").show();
+			$("#mbappt").show();
 			$("#muinfo").click();
+			
+			toastNotify("Logged in successfully");
 			
 			//Fetch upcoming appointments.
 			//Note: could be replaced in the future with a general funct. to retrieve all data necessary to be displayed post-login.
@@ -34,16 +43,16 @@ $(document).ready(function() {
 	
 	//Response from Register attempts
 	socket.on('register_res', function(data) {
-		if(data.status == 'error') console.log(data.reason);
+		if(data.status == 'error') toastNotify(data.reason);
 		else{	
-			console.log("You have been registered successfully");
+			toastNotify("Account registered succesfully");
 			socket.emit('login_res');
 		}
 	});  
 	
 	//Response to upcoming appointments request.
 	socket.on('fetch_future_appointments_res', function(data) {
-		if(data.status == 'error') console.log(data.reason);
+		if(data.status == 'error') toastNotify(data.reason);
 		else{	
 			console.log("Upcoming appointments fetched.");
 			future_appts = data.result;
@@ -51,7 +60,7 @@ $(document).ready(function() {
 	});
 	
 	socket.on('get_user_info_res', function(data) {
-        if(data.status == 'error') console.log(data.reason);
+        if(data.status == 'error') toastNotify(data.reason);
         else{    
             console.log("User info fetched.");
             user_info = data.result;
@@ -59,13 +68,18 @@ $(document).ready(function() {
     });
 
     socket.on('fetch_dentist_appointments_res', function(data) {
-        if(data.status == 'error') console.log(data.reason);
+        if(data.status == 'error') toastNotify(data.reason);
         else{    
             console.log("Dentist appointments fetched.");
             dentist_apps = data.result;
         }
     });
 });
+
+function toastNotify(txt){
+	$("#toast").text(txt);
+	$("#toast").finish().show().delay(4000).fadeOut("slow");
+}
 
 function swapTo(section, btn){
 	$("#main section").hide();
@@ -76,179 +90,108 @@ function swapTo(section, btn){
 
 function addPhone(btn){
 	if(++phone_counter < 4){
-		$(btn).after("<br><input type=\"password\" placeholder=\"123-456-7890\" class=\"input\"> <button class=\"small\" onclick=\"addPhone(this);\">+</button>");
+		$(btn).after("<br><input type=\"text\" placeholder=\"123-456-7890\" class=\"phone\"> <button class=\"small\" onclick=\"addPhone(this);\">+</button>");
 		if(phone_counter >= 3) $("#register button.small").remove();
 	}
 }
 
-function login(){
-	socket.emit('login', {email: $("#lgemail").val(), password: $("#lgpass").val()});
-}
-
-function LoginformValidation()
-{
-	
-	var lgemail = document.getElementById("lgemail");
-	var lgpass = document.getElementById("lgpass");
-	if(ValidateEmail(lgemail))
-	{
-		if(pass_validation(lgpass,7,20))
-		   { } }
-	return false;
-}
-
-function RegisterformValidation()
-{
-var rfirstname = document.getElementById("rfirstname");
-var rmiddlename = document.getElementById("rmiddlename");
-var rlastname = document.getElementById("rlastname");
-
-var rdateofbirth = document.getElementById("rdateofbirth");
-var rssn = document.getElementById("rssn");
-
-var rstreetn = document.getElementById("rstreetn");
-var rstreetname = document.getElementById("rstreetname");
-var rcity = document.getElementById("rcity");
-var rprovince = document.getElementById("rprovince");
-
-var remail = document.getElementById("remail");
-var rpass = document.getElementById("rpass");
-var rpassconf = document.getElementById("rpassconf");
-var rphone = document.getElementById("rphone");
-
-
-if(allLetter(rfirstname) && allLetter(rlastname) && allLetter(rmiddlename) && allLetter(rstreetname) 
-&& allLetter(rcity) && allLetter(rprovince) )
-{
-if(dateofbirth_validation(rdateofbirth))
-{
-if(allnumeric(rssn) && allnumeric(rstreetn))
-{	
-if(ValidateEmail(remail))
-{
-if(pass_validation(rpass,7,20) )
-{
-if(passconf_validation(rpass, rpassconf))
-{ 
-if(phone_validation(rphone))
-{ 	
-}} }} }} }
-return false;
-}
-
-function dateofbirth_validation(rdateofbirth)
-{
-	var regex = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
- 
-        //Check whether valid dd/MM/yyyy Date Format.
-        if (regex.test(rdateofbirth)) {
-            var parts = rdateofbirth.split("/");
-            var dtDOB = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
-            var dtCurrent = new Date();
-        
-            if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
-                return false;
-            }
- 
-            if (dtCurrent.getFullYear() - dtDOB.getFullYear() == 18) {
-                if (dtCurrent.getMonth() < dtDOB.getMonth()) {
-                    return false;
-                }
-                if (dtCurrent.getMonth() == dtDOB.getMonth()) {
-                    
-                    if (dtCurrent.getDate() < dtDOB.getDate()) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        } else {
-            alert("Enter date in dd/MM/yyyy format ONLY.");
-			rdateofbirth.focus();
-            return false;
-        }
-}
-
-function allnumeric(x)
-{ 
-var numbers = /^[0-9]+$/;
-if(x.value.match(numbers))
-{
-return true;
-}
-else
-{
-alert('SSN and street No must have numeric characters only');
-return false;
-}
-}
-function pass_validation(passid,mx,my)
-{
-var passid_len = passid.value.length;
-if (passid_len == 0 ||passid_len >= my || passid_len < mx)
-{
-alert("Password should not be empty / length be between "+mx+" to "+my);
-passid.focus();
-return false;
-}
-return true;
-}
-
-function allLetter(uname)
-{ 
-var letters = /^[A-Za-z]+$/;
-if(uname == null) {
-	alert('Firstname, lastName,middle name, city, province and street name must have alphabet characters only');
-	uname.focus();
-	return false;
-}
-if(uname.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('Firstname, lastName,middle name, city, province and street name must have alphabet characters only');
-uname.focus();
-return false;
-}
-}
-
-
-function ValidateEmail(remail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(remail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-remail.focus();
-return false;
-}
-}
-
-function passconf_validation(rpass, rpassconf) {
-	if(rpass == rpassconf) {
+function valEmail(email){
+	if($(email).val().match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))
 		return true;
-	}
-	else{
-		alert("The two passwords entered are different");
-	}
-
+	
+	toastNotify("Invalid email address");
+	email.focus();
+	return false;
 }
 
-function phone_validation(rphone) {
-	var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+function valPassword(password){
+	var passid_len = $(password).val().length;
+	if (passid_len >= 7 && passid_len <= 20)
+		return true;
+	
+	toastNotify("Password length should be between 7 and 20");
+	password.focus();
+	return false;
+}
 
-	if(re.test(rphone)) {
-		return true
+function valAlpha(name){ 
+	if($(name).val().match(/^[A-Za-z]+$/))
+		return true;
+	
+	toastNotify('Letters-only value expected');
+	name.focus();
+	return false;
+}
+
+function login(){
+	var lgemail = $("#lgemail");
+	var lgpass = $("#lgpass");
+	
+	if(valEmail(lgemail) && valPassword(lgpass))
+		socket.emit('login', {email: lgemail.val(), password: lgpass.val()});
+}
+
+function register(){
+	var rtype = $("#rtype").val();
+	var rfirstname = $("#rfirstname");
+	var rmiddlename = $("#rmiddlename");
+	var rlastname = $("#rlastname");
+	var rdateofbirth = $("#rdateofbirth");
+	var rssn = $("#rssn");
+	var rstreetn = $("#rstreetn");
+	var rstreetname = $("#rstreetname");
+	var rcity = $("#rcity");
+	var rprovince = $("#rprovince");
+	var remail = $("#remail");
+	var rpass = $("#rpass");
+	var rpassconf = $("#rpassconf");
+
+	if(rtype == "patient" && (new Date(Date.now() - Date.parse(rdateofbirth.val())).getUTCFullYear() - 1970) < 15){
+		toastNotify('Registering for users under 15 is a WIP feature.');
+		rdateofbirth.focus(); //TODO: change this  the guardian SSN thing
+		return;
 	}
-	else{
-		alert("You have entered an invalid phone number");
-	};
+	
+	if(!rssn.val().match(/^[0-9]{3}-[0-9]{2}-[0-9]{4}$/)){ 
+		toastNotify('SSNs must be of the format 012-34-5678');
+		rssn.focus();
+		return;
+	}
+	
+	if(!rstreetn.val().match(/^[0-9]+$/)){
+		toastNotify('Numeric value expected');
+		rstreetn.focus();
+		return;
+	}
+	
+	if(rpass.val() != rpassconf.val()){
+		toastNotify('Passwords do not match');
+		rpassconf.focus();
+		return;
+	}
+	
+	//validates each phone number
+	var failed = false;
+	$(".phone").each(function(){
+		if($(this).val() != "" && !$(this).val().match(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/)){
+			toastNotify("Invalid phone number");
+			$(this).focus();
+			failed = true; //can't return here, we're inside an anonymous funct.
+		}
+	}); if(failed) return;
+	
+	//put each value in an array
+	var rphones = $(".phone").map(function(){
+		return $(this).val() == "" ? null : $(this).val();
+	}).get();
+
+	if(valAlpha(rfirstname) && valAlpha(rlastname) && (rmiddlename.val() == "" || valAlpha(rmiddlename)) 
+		&& valAlpha(rstreetname) && valAlpha(rcity) && valAlpha(rprovince)
+		&& valEmail(remail) && valPassword(rpass)){
+		socket.emit('register', {type: rtype, fname: rfirstname.val(), mname: rmiddlename.val(), lname: rlastname.val(), 
+								 dob: rdateofbirth.val(), ssn: rssn.val(), streetn: rstreetn.val(), street: rstreetname.val(), 
+								 city: rcity.val(), prov: rprovince.val(), email: remail.val(), password: rpass.val(), phones: rphones});
+	}
 }
 
 function book_appt(){
